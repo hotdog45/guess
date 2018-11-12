c26309d880ea500b5d16a806593c79210b31466c
李顺风 <mac2020@126.com>
2018-11-12 17:36:32 +0800

    ui

--- src/views/caiwu/guess.vue
+++ src/views/caiwu/guess.vue
@@ -14,6 +14,7 @@
         <img class="public" src="@/assets/images/public.png">
       </div>
       <div class="publicLab">发布</div>
+      
       <div class="bodyBtn">
         <div class="btn1">
           <img  src="@/assets/images/me.png">
@@ -23,9 +24,11 @@
           <img src="@/assets/images/share.png">
           <div>208</div>
         </div>
-        <div>
-          <img style="width: 36px;height: 36px;" src="@/assets/images/icon2.png">
-          <div>208</div>
+        <div class="tipdiv">
+          <div class="tip2"  style="text-align: left;">三个字</div>
+          <img style="width: 37px;height: 37px;" src="@/assets/images/icon2.png">
+          <div class="tip" style="color:#000;font-size=11px;">提示</div>
+          
         </div>
 
       </div>
@@ -36,13 +39,15 @@
         <div class="send">发送</div>
 
       </div>
-      <marquee class="desc"  width=90%  direction=left align=middle>弹来弹去跑马灯!</marquee>
+      <marquee class="desc"  width=90%  direction=left align=middle>
+        @虎嗅【2018天猫双11战绩：成交额2135亿元，物流单量超10亿】双11开启当晚，
+        2分零5秒成交额超过100亿元；一小时达到672.6亿元（接近100亿美金）；15小时49分39秒，
+        破2017年11月11日全天交易额；22小时28分37秒，成交额突破2000亿元。
+
+      </marquee>
     </div>
     </div>
     
-
-
-
   </div>
 </template>
 
@@ -98,7 +103,7 @@ export default {
 .view {
   z-index: 1000;
   position: fixed;
-  background: rgba(0, 0, 0, 0.1);
+  background: rgba(90, 90, 90, 1);
   bottom: 0;
   left: 0;
   right: 0;
@@ -132,10 +137,14 @@ export default {
 }
 
 .bodyBtn {
-  padding: 216px 319px 0 319px;
+  z-index: 900;
+  position: fixed;
+  left: 310px;
+  bottom: 140px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  z-index: 1003;
 }
 .bodyBtn div {
   display: flex;
@@ -144,13 +153,37 @@ export default {
 }
 
 .bodyBtn div img {
-  width: 28px;
-  height: 28px;
+  width: 25px;
+  height: 25px;
+  z-index: 1006;
 }
 .bodyBtn div div {
   font-size: 11px;
+  color: #fff;
+  padding-top: 5px;
+  padding-bottom: 20px;
 }
 
+.tipdiv {
+  position: relative;
+  z-index: 1003;
+}
+.tip {
+  position: absolute;
+  top: 5px;
+  z-index: 1014;
+}
+.tip2 {
+  position: absolute;
+  top: 6px;
+  left: -70px;
+  font-size: 11px;
+  background: #000;
+  border-radius: 12px;
+  width: 85px;
+  height: 22px;
+  z-index: 999;
+}
 .desc {
   color: #fff;
 }
@@ -165,19 +198,40 @@ export default {
   position: fixed;
   bottom: 51px;
   left: 24px;
+  width: 59px;
+  height: 81px;
 }
 .name {
   z-index: 1000;
   position: fixed;
   bottom: 60px;
   left: 27px;
-  width: 324;
+  width: 324px;
+  padding: 0 66px;
   line-height: 43px;
+  border-radius: 22px;
+  border: none;
+  background: #000;
+  color: #fff;
 }
 .send {
   z-index: 1000;
   position: fixed;
-  bottom: 71px;
-  right: 54px;
+  bottom: 62px;
+  right: 20px;
+  color: #fff;
+  font-size: 15px;
+  padding: 20px 20px 10px 10px;
+}
+
+.desc {
+  z-index: 1000;
+  position: fixed;
+  bottom: 13px;
+  right: 27px;
+  left: 27px;
+  width: 318px;
+  height: 21px;
+  font-size: 15px;
 }
 </style>
--- src/views/caiwu/myguess.vue
+++ src/views/caiwu/myguess.vue
@@ -1,28 +1,182 @@
 <template>
-    <div>{{message}}</div>
+    <div>
+        <!--{{message}}-->
+        <div class="top-bar">我的猜物
+            <div class="img-rapper"><img class='back' src="@/assets/images/back.png" alt=""></div>
+        </div>
+        
+        <div class="content">
+            <div class="left">
+                <img src="" alt="">
+            </div>
+            <div class="right">
+                <div class="intro">
+                    <div class="avator">
+                        <img src="" alt="">
+                    </div>
+                    <div class="intro-detail">
+                        <div class="intro-name">大豆</div>
+                        <div class="intro-address">北京-西城区</div>
+                    </div>
+                    <!--<p class="intro-name">大豆</p>-->
+                    <!--<p class="intro-address">北京-西城区</p>-->
+                </div>
+                <div class="join">1255人参与</div>
+                <div class="join">正确答案:房车</div>
+                <div class="btn-rapper">
+                    <div class="right-btn success">你猜对了</div>
+                    <div class="right-btn detail-talk">细聊</div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="left">
+                <img src="" alt="">
+            </div>
+            <div class="right">
+                <div class="intro">
+                    <div class="avator">
+                        <img src="" alt="">
+                    </div>
+                    <div class="intro-detail">
+                        <div class="intro-name">大豆</div>
+                        <div class="intro-address">北京-西城区</div>
+                    </div>
+                    <!--<p class="intro-name">大豆</p>-->
+                    <!--<p class="intro-address">北京-西城区</p>-->
+                </div>
+                <div class="join">1255人参与</div>
+                <div class="join">正确答案:房车</div>
+                <div class="btn-rapper">
+                    <div if='' class="right-btn success">你猜对了</div>
+                    <div class="right-btn success">你猜错了</div>
+                    <div class="right-btn detail-talk">细聊</div>
+                    
+                    <div class="right-btn detail-talk">细聊</div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
+// import MyPublic from "./mypublic.vue";
+// import { ButtonTab, ButtonTabItem } from "vux";
 
-    export default {
-        components: {},
-        data() {
-            return {
-                message: "详情页",
-            };
-        },
-
-        methods: {},
-
-        //created创建完毕状态
-        created() {
-        },
-
-
+export default {
+  data() {
+    return {
+      message: "详情页11"
     };
+  },
+
+  methods: {},
+
+  //created创建完毕状态
+  created() {},
+  components: {}
+};
 </script>
 
-<style scoped lang="less" rel="stylesheet/less">
+<style lang="less" rel="stylesheet/less" scoped>
+.top-bar {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 56px;
+  line-height: 56px;
+  font-size: 15px;
+}
+.img-rapper {
+  position: absolute;
+  top: 0;
+  left: 14px;
+  padding-top: 5px;
+  width: 56px;
+  height: 56px;
+}
+.back {
+  width: 20px;
+  height: 20px;
+}
+.tab {
+  margin: 0 30px;
+}
+.content {
+  display: flex;
+  flex-direction: row;
+  margin: 0px 20px;
+  padding: 30px 0;
+  border-bottom: #111 solid 1px;
+}
+.left {
+  width: 120px;
+  height: 200px;
+  background: orange;
+}
+.left img {
+  width: 100%;
+  height: 100%;
+}
+.right {
+  position: relative;
+  right: 0;
+  flex: 1;
+  margin: 0 15px;
+}
+.intro {
+  display: flex;
+  flex-direction: row;
+}
+.intro .avator {
+  width: 44px;
+  height: 44px;
+  border-radius: 50%;
+  background: red;
+  margin-right: 10px;
+}
+.intro .intro-detail {
+  padding-top: 10px;
+}
+.intro .intro-detail div {
+  display: inline;
+}
+.intro .intro-detail .intro-name {
+  font-weight: bold;
+  font-size: 13px;
+}
+.intro .intro-detail .intro-address {
+  font-size: 12px;
+}
+.join {
+  text-align: right;
+}
+.btn-rapper {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+.right-btn {
+  width: 130px;
+  height: 40px;
+  line-height: 40px;
+  background: pink;
+  border-radius: 30px;
+  text-align: center;
+}
+.success {
+  margin: 10px 0;
+  background: #fff;
+  border: #53ffdc solid 1px;
+}
 
-
+.detail-talk {
+  margin: 10px 0;
+  background: #53ffdc;
+}
 </style>
