18b297b49a83e773f41647c423267318b1e11ea3
李顺风 <mac2020@126.com>
2018-12-13 13:29:56 +0800

    进度条 路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,16 @@ import Router from "vue-router";
 const asyncImport = file => () => import(`@/views/${file}`);
 
 Vue.use(Router);
-
+const myguess = resolve => require(['@/views/caiwu/myguess'], resolve)
+const guess = resolve => require(['@/views/caiwu/guess'], resolve)
+const pub = resolve => require(['@/views/caiwu/public'], resolve)
+const details = resolve => require(['@/views/caiwu/details'], resolve)
 export const constantRouterMap = [
-  { path: "/myguess", component: asyncImport("caiwu/myguess") },
-  { path: "/guess", component: asyncImport("caiwu/guess") },
-  { path: "/public", component: asyncImport("caiwu/public") },
-  { path: "/details", component: asyncImport("caiwu/details") },
-  { path: "/test", component: asyncImport("caiwu/test") },
+  { path: "/myguess", component: myguess },
+  { path: "/guess", component: guess },
+  { path: "/public", component: pub },
+  { path: "/details", component: details },
+  // { path: "/test", component: asyncImport("caiwu/test") },
 
   { path: "*", redirect: "/404", hidden: true }
 ];
